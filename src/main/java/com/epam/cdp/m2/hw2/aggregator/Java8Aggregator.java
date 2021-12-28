d38415f05dd86c90ba5f4616743fa2a2cfc71d53
Mikhail Zaks <mikhail_zaks@epam.com>
2021-12-28 14:00:25 +0300

    add all methods Java8Aggregator

--- src/main/java/com/epam/cdp/m2/hw2/aggregator/Java8Aggregator.java
+++ src/main/java/com/epam/cdp/m2/hw2/aggregator/Java8Aggregator.java
@@ -3,21 +3,52 @@ package com.epam.cdp.m2.hw2.aggregator;
 import javafx.util.Pair;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Java8Aggregator implements Aggregator {
 
     @Override
-    public int sum(List<Integer> numbers) {
-        throw new UnsupportedOperationException();
+    public long sum(List<Integer> numbers) {
+        return numbers.stream().mapToLong(a -> a).sum();
     }
 
     @Override
     public List<Pair<String, Long>> getMostFrequentWords(List<String> words, long limit) {
-        throw new UnsupportedOperationException();
+        return words.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .map(e -> new Pair<String, Long>(e.getKey(), e.getValue()))
+                .sorted((o1, o2) -> {
+                    long compare = o2.getValue() - o1.getValue();
+                    if (compare == 0) {
+                        return o1.getKey().compareTo(o2.getKey());
+                    }
+                    return (int) compare;
+                })
+                .limit(limit)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<String> getDuplicates(List<String> words, long limit) {
-        throw new UnsupportedOperationException();
+        return words.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(w -> w.getValue() > 1)
+                .map(Map.Entry::getKey)
+                .sorted((o1, o2) -> {
+                    int compare = o1.length() - o2.length();
+                    if (compare == 0) {
+                        return o1.compareTo(o2);
+                    }
+                    return compare;
+                })
+                .limit(limit)
+                .collect(Collectors.toList());
     }
 }
