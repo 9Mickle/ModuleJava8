4d8d0eb719ab2abb4635a87344f43b94f78c4082
Mikhail Zaks <mikhail_zaks@epam.com>
2021-12-28 13:58:49 +0300

    add all methods Java7ParallelAggregator

--- src/main/java/com/epam/cdp/m2/hw2/aggregator/Java7ParallelAggregator.java
+++ src/main/java/com/epam/cdp/m2/hw2/aggregator/Java7ParallelAggregator.java
@@ -2,22 +2,155 @@ package com.epam.cdp.m2.hw2.aggregator;
 
 import javafx.util.Pair;
 
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 public class Java7ParallelAggregator implements Aggregator {
 
     @Override
-    public int sum(List<Integer> numbers) {
-        throw new UnsupportedOperationException();
+    public long sum(List<Integer> numbers) {
+
+        long result = 0;
+        Future<Long> res1 = new FutureTask<>(new Callable<Long>() {
+            @Override
+            public Long call() {
+                long res = 0;
+                for (int i = 0; i < numbers.size() / 2; i++) {
+                    res += numbers.get(i);
+                }
+                return res;
+            }
+        });
+
+        Future<Long> res2 = new FutureTask<>(new Callable<Long>() {
+            @Override
+            public Long call() {
+                long res = 0;
+                for (int i = numbers.size() / 2; i < numbers.size(); i++) {
+                    res += numbers.get(i);
+                }
+                return res;
+            }
+        });
+
+        try {
+            new Thread((Runnable) res1).start();
+            new Thread((Runnable) res2).start();
+            result = res1.get() + res2.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
     public List<Pair<String, Long>> getMostFrequentWords(List<String> words, long limit) {
-        throw new UnsupportedOperationException();
+        List<Pair<String, Long>> list = new ArrayList<>();
+        for (String word: words) {
+            int count = Collections.frequency(words, word);
+            list.add(new Pair<>(word, (long) count));
+        }
+
+        Set<Pair<String, Long>> set = new HashSet<>(list);
+        List<Pair<String, Long>> sortedList = new ArrayList<>(set);
+
+        Collections.sort(sortedList, new Comparator<Pair<String, Long>>() {
+            @Override
+            public int compare(Pair<String, Long> o1, Pair<String, Long> o2) {
+                long compare = o2.getValue() - o1.getValue();
+                if (compare == 0) {
+                    return o1.getKey().compareTo(o2.getKey());
+                }
+                return (int) compare;
+            }
+        });
+
+        List<Pair<String, Long>> result = new ArrayList<>();
+        if (limit > sortedList.size()) {
+            result.addAll(sortedList);
+        } else {
+            for (int i = 0; i < limit; i++) {
+                result.add(sortedList.get(i));
+            }
+        }
+        return result;
     }
 
     @Override
     public List<String> getDuplicates(List<String> words, long limit) {
-        throw new UnsupportedOperationException();
+
+        Future<List<String>> upCaseFirstHalf = new FutureTask<>(new Callable<List<String>>() {
+            @Override
+            public List<String> call() {
+                List<String> list = new ArrayList<>();
+                for (int i = 0; i < words.size() / 2; i++) {
+                    list.add(words.get(i).toUpperCase());
+                }
+                return list;
+            }
+        });
+
+        Future<List<String>> upCaseSecondHalf = new FutureTask<>(new Callable<List<String>>() {
+            @Override
+            public List<String> call() {
+                List<String> list = new ArrayList<>();
+                for (int i = words.size() / 2; i < words.size(); i++) {
+                    list.add(words.get(i).toUpperCase());
+                }
+                return list;
+            }
+        });
+
+        Future<List<String>> getDuplicates = new FutureTask<>(new Callable<List<String>>() {
+            @Override
+            public List<String> call() throws ExecutionException, InterruptedException {
+                List<String> listUp = new ArrayList<>(upCaseFirstHalf.get());
+                listUp.addAll(upCaseSecondHalf.get());
+                Set<String> set = new HashSet<>();
+                for (String word: listUp) {
+                    if (Collections.frequency(listUp, word) > 1) {
+                        set.add(word);
+                    }
+                }
+                listUp = new ArrayList<>(set);
+                return listUp;
+            }
+        });
+
+        new Thread((Runnable) upCaseFirstHalf).start();
+        new Thread((Runnable) upCaseSecondHalf).start();
+        new Thread((Runnable) getDuplicates).start();
+
+
+        List<String> sortedList = null;
+        try {
+            sortedList = new ArrayList<>(getDuplicates.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Collections.sort(sortedList, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                long compare = o1.length() - o2.length();
+                if (compare == 0) {
+                    return o1.compareTo(o2);
+                }
+                return (int) compare;
+            }
+        });
+
+        List<String> limitList = new ArrayList<>();
+        if (limit > sortedList.size()) {
+            limitList.addAll(sortedList);
+        } else {
+            for (int i = 0; i < limit; i++) {
+                limitList.add(sortedList.get(i));
+            }
+        }
+        return limitList;
     }
 }
